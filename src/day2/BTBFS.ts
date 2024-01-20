4357baa680b718759bc3c71467ea4a13db62dbb3
kurobane <darkflamemaster663@gmail.com>
2024-01-20 16:07:33 -0300

    btbfs

--- src/day2/BTBFS.ts
+++ src/day2/BTBFS.ts
@@ -1,3 +1,26 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+//use a queue to be O(n) time, javascript array makes the tree O(n^2) time because of the shift() method
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: BinaryNode<number>[] | null = [head];
+
+    while (q.length) {
+        const current = q.shift() as BinaryNode<number> | undefined | null;
+
+        if (!current) {
+            continue;
+        }
+
+        if (current.value === needle) {
+            return true;
+        }
+
+        if (current.left) {
+            q.push(current.left);
+        }
+
+        if (current.right) {
+            q.push(current.right);
+        }
+    }
+    return false;
 }
